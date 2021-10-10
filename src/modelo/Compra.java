6939a5d26075c2edb6cee071042f481d2331bda7
martin-2k <85652994+martin-2k@users.noreply.github.com>
2021-10-09 20:40:03 -0500

    arreglo
    hi

--- src/modelo/Compra.java
+++ src/modelo/Compra.java
@@ -7,47 +7,39 @@ public class Compra {
 	private int idCompra;
 	private Cliente cliente;
 	private ArrayList<Entrada> entradas;
-	
+
 	public Compra(Cliente cliente) {
 		this.cliente = cliente;
 		idCompra = numCompras;
 		numCompras += 1;
 		entradas = new ArrayList<Entrada>();
 	}
-	
+
 	public int getIdCompra() {
 		return idCompra;
 	}
 	
-	public Cliente getCliente() {
-		return cliente;
-	}
-	/*
-	 * public Cliente getCliente() { return cliente; }
-	 */
-	
+	public Cliente getCliente() { return cliente; }
+	 
 	public void agregarEntrada(Entrada entrada) {
 		entradas.add(entrada);
 	}
-	
-	public String toString() { 
+
+	public String toString() {
 		String imprimible = "";
-		for (Entrada entrada : entradas)
-		{
+		for (Entrada entrada : entradas) {
 			imprimible += entrada.toString();
 			imprimible += "\n";
 		}
 		return imprimible;
 	}
-	
+
 	public int getPrecio() {
 		int precio;
-		for (Entrada entrada : entradas)
-		{
+		for (Entrada entrada : entradas) {
 			precio += entrada.getPrecio();
 		}
 		return precio;
 	}
-	
 
 }
